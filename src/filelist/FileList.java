cefec8af1c68bbe1871b6cc7902968ca2c885565
Tatyana Galeyev <tanuwka.spb@gmail.com>
2011-10-24 18:07:01 +0400

    1ver

---
+++ src/filelist/FileList.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filelist;
+
+/**
+ *
+ * @author Администратор
+ */
+public class FileList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GroovyFileList gfl = new GroovyFileList();
+        gfl.run();
+    }
+}
