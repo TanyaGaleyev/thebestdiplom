89adabba778902c369e67072a87b89f31343c3db
Tatyana <tanuwka.spb@gmail.com>
2011-11-15 19:57:45 +0300

    Update src/main/filelist/Config.groovy

--- src/main/filelist/Config.groovy
+++ src/main/filelist/Config.groovy
@@ -20,16 +20,5 @@ class Config {
             dbuser = dbu;
         }
         
-    def boolean equals1(Object obj) {
-        if(this == obj) return true;
-        if(obj == null) return false;
-        if (!obj instanceof Config) return false;
-        
-        def Config other = obj;
-        if(!dataBase.equals(other.dataBase)) return false;
-        if(!dbname.equals(other.dbname)) return false;
-        if(!dbuser.equals(other.dbuser)) return false;
-        return true;
-    }
 }
 
